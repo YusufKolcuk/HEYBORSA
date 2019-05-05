46f8b49e0b7fec817dd771ac67e7db4855a2b40d
YusufKolcuk <37012068+YusufKolcuk@users.noreply.github.com>
2019-05-05 16:53:12 +0300

    V4

--- heyborsa-api/src/main/java/com/heyborsa/service/KullaniciService.java
+++ heyborsa-api/src/main/java/com/heyborsa/service/KullaniciService.java
@@ -2,11 +2,13 @@ package com.heyborsa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.heyborsa.dao.KullaniciDAO;
 import com.heyborsa.entity.Kullanici;
 
 @Service
+@Transactional
 public class KullaniciService {
 	@Autowired
 	KullaniciDAO kullaniciDAO;
